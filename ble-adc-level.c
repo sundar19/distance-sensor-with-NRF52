194bcba49e9d416a73359f99a6e567b3cf89170e
G.Sundar Santhanam <31473503+sundar19@users.noreply.github.com>
2020-02-03 17:38:15 +0530

    Update ble-adc-level.c

--- ble-adc-level.c
+++ ble-adc-level.c
@@ -796,7 +796,7 @@ void saadc_init(void)
 {
     ret_code_t err_code;
     nrf_saadc_channel_config_t channel_config =
-        NRF_DRV_SAADC_DEFAULT_CHANNEL_CONFIG_SE(NRF_SAADC_INPUT_AIN1);
+        NRF_DRV_SAADC_DEFAULT_CHANNEL_CONFIG_SE(NRF_SAADC_INPUT_AIN0);
 
     err_code = nrf_drv_saadc_init(NULL, saadc_callback);
     APP_ERROR_CHECK(err_code);
